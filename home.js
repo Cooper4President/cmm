a33b98a59e06530c6cf9b880eb64fac12cfd173a
OliverEhrhardt <oliver.ehrhardt0@gmail.com>
2016-02-28 21:37:01 -0700

    added --date functionality to chat

--- home.js
+++ home.js
@@ -1,11 +1,16 @@
 //main function
 $(document).ready(function(){
      $(".sub").on("click", function(cEvent){
-     	var cmdVal = $(".cmd").val();
-	 	if(cmdVal != ""){submit()}
-
+     	submit();
      });
 });
+
+//checks input for command dilimiter
+function commandCheck(val){
+	if(/^--/.test(val)){
+		return true;
+	}else return false;
+}
 
 //Enter key functionality for sumbitting messages
 $(document).delegate('input:text','keypress',function(e) {
@@ -17,8 +22,39 @@ $(document).delegate('input:text','keypress',function(e) {
 
 //client side submit function
 function submit(){
-   
-	$(".chat-container").append("<div> USER: " + $(".cmd").val() + "</div>");
-	$(".cmd").val("");
-   
+	var inp = $(".cmd").val();
+	if(inp != ""){
+		if(!commandCheck(inp)){ //checks if command
+			$(".chat-container").append("<div> USER: " + inp + "</div>");
+		}else{
+			parseCommand(inp);
+		}
+		$(".cmd").val("");
+	}
 };
+
+//parses command for functionality
+function parseCommand(cmd){
+	command = cmd.replace('--','');
+	if(command == "date"){
+		var date = getTodaysDate();
+		$(".chat-container").append("<div> COMMAND: Current Date: " + date + "</div>");
+	}
+}
+
+//parses today's date
+function getTodaysDate(){
+	var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1; //January is 0!
+
+    var yyyy = today.getFullYear();
+    if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+    var today = mm+'/'+dd+'/'+yyyy;
+    return today;
+}
