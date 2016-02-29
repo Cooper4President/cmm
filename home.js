5dc68791a92835ce0d0e60e70e06b4ce70679c83
OliverEhrhardt <oliver.ehrhardt0@gmail.com>
2016-02-28 20:36:48 -0700

    added comments for clairity

--- home.js
+++ home.js
@@ -1,10 +1,13 @@
+//main function
 $(document).ready(function(){
      $(".sub").on("click", function(cEvent){
-	 submit();
+     	var cmdVal = $(".cmd").val();
+	 	if(cmdVal != ""){submit()}
 
      });
 });
 
+//Enter key functionality for sumbitting messages
 $(document).delegate('input:text','keypress',function(e) {
     if (e.which === 13) {
 	e.preventDefault();
@@ -12,6 +15,7 @@ $(document).delegate('input:text','keypress',function(e) {
     };
 });
 
+//client side submit function
 function submit(){
    
 	$(".chat-container").append("<div> USER: " + $(".cmd").val() + "</div>");
