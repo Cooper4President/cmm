c058ba90695816fb316688cb2e0f226af026b7fd
Cooper4President <Cooper4President@users.noreply.github.com>
2016-03-04 15:20:59 -0700

    Started adding serverside login/authentication functionality

--- client/index-sockets.js
+++ client/index-sockets.js
@@ -2,8 +2,21 @@ var socket = io();
 
 
 function sendUser(user, pass){
-	socket.emit('login',user);
+	socket.emit('login attempt', { username: user, password: pass });
 }
+
+socket.on('login success', function(token){
+	//user login credentials accepted, server has sent an auth token
+	//save the auth token locally in a cookie
+	$.cookie('authToken', token, { path: '/' });
+});
+
+socket.on('login fail', function(){
+	//user login credentials were not accepted by the server
+
+	//TEMPORARY. Replace this with whatever you want to happen when login fails.
+	alert('Login failed! Check username/password and make sure account exists.');
+});
 
 socket.on('page load', function(){
 	$(location).attr('href',"http://localhost:3000/chat.html");
--- server-main.js
+++ server-main.js
@@ -5,8 +5,11 @@ Then, go to 'localhost:####' in browser, where #### is the port number
 
 //for the 'express' library
 var express = require('express');
-var handlebars = require('handlebars');
 var app = express();
+//handlebars framework
+var handlebars = require('handlebars');
+//nodejs crypto module
+var crypto = require('crypto');
 /*
 tell express to use the 'client' folder as a source of static files such as
 index.html, index.css, etc...
@@ -23,6 +26,15 @@ var fs = require('fs');
 var portNum = 3000;
 //number of active socket connections
 var numConnections = 0;
+//active session tokens and their associated usernames
+var activeUsers = {};
+
+//TEMPORARY. Used for testing until the actual database is ready.
+var TESTINGuserAccounts = {};
+TESTINGuserAccounts['johnsmith'] = 'mypass123';
+TESTINGuserAccounts['hello'] = 'world';
+TESTINGuserAccounts['1337hacker'] = 'p@ssw0rd';
+TESTINGuserAccounts['student3'] = 'thisISaPASSWORD';
 
 
 //this is called when a socket is connected
@@ -35,9 +47,41 @@ io.on('connection', function(socket){
   console.log('total connected users: ' + numConnections);
 
   //login event
-  socket.on('login',function(data){
-    fs.writeFile('client/user.txt', data, function(err){err ? console.log(err) : console.log('user file saved')});
-    io.emit('page load');
+  socket.on('login attempt',function(userInfo){
+    console.log('user attempting to login...\n' +
+    'user: ' + userInfo.username + ' pass: ' + userInfo.password);
+
+    var loginSuccess = false;
+    for(var key in TESTINGuserAccounts){
+      //if the username exists in the database...
+      if(key == userInfo.username){
+        //if the password matches the username...
+        if(TESTINGuserAccounts[key] == userInfo.password){
+          loginSuccess = true;
+          break;
+        }
+      }
+    }
+
+    if(loginSuccess){
+      //TEMPORARY. Generate random hash to be the 'token' for this user session
+      var randStr = Math.random().toString();
+      var token = crypto.createHash('md5').update(randStr).digest('hex');
+      //add user to list of active sessions
+      activeUsers[token] = userInfo.username;
+      console.log('generated token: ' + token +
+      ' for user: ' + userInfo.username);
+      //give client the token
+      io.emit('login success', token);
+      //redirect client to chat page
+      io.emit('page load');
+    }
+    else{
+      //login failed
+      io.emit('login fail');
+      console.log('login failed...\n' + 'user: ' + userInfo.username +
+      ' pass: ' + userInfo.password);
+    }
   });
 
   //called when socket is disconnected
