a841f442868e9ff927b80d53be34a59d5f56a3d9
OliverEhrhardt <oliver.ehrhardt0@gmail.com>
2016-03-18 12:27:53 -0600

    divided chat.css into menu.css for orginization

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,12 @@ module.exports = function(grunt) {
                 src: 'client/css/chat.css',
                 dest: 'client/css/chat-prefix.css'
             },
+
+            menu: {
+                src: 'client/css/menu.css',
+                dest: 'client/css/menu-prefix.css'
+            },
+
             index: {
                 src: 'client/css/index.css',
                 dest: 'client/css/index-prefix.css'
