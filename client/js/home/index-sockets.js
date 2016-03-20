4bc9b327a3777c90c8110f735e685cc8a6781074
Cooper4President <Cooper4President@users.noreply.github.com>
2016-03-19 19:21:39 -0600

    moving server changes to main branch

--- client/js/home/index-sockets.js
+++ client/js/home/index-sockets.js
@@ -1,11 +1,26 @@
 define(['socket_io'], function(io){
+
 	var socket = io();
-	socket.on('login success', function(token){
-	//user login credentials accepted, server has sent an auth token
-	//save the auth token locally in a cookie
-	$.cookie('authToken', token, { path: '/' });
+
+
+	//sent by server when attempt to create new user account succeeds
+	socket.on('account create success', function(){
+		//todo
 	});
 
+	//sent by server when attempt to create neww user account fails
+	socket.on('account create fail', function(){
+		//todo
+	});
+
+	//sent by server when login attempt succeeds
+	socket.on('login success', function(token){
+		//user login credentials accepted, server has sent an auth token
+		//save the auth token locally in a cookie
+		$.cookie('authToken', token, { path: '/' });
+	});
+
+	//sent by server when login attempt fails
 	socket.on('login fail', function(){
 		//user login credentials were not accepted by the server
 
@@ -13,15 +28,24 @@ define(['socket_io'], function(io){
 		alert('Login failed! Check username/password and make sure account exists.');
 	});
 
+	//redirect user to the chat page
 	socket.on('page load', function(){
-		$(location).attr('href',"http://localhost:3000/chat.html");
+		$(location).attr('href', '/chat.html');
 	});
 
+
 	return{
-		sendUser: function(user, pass){
+		//send request to server asking to create new user account
+		//the server will send back either a success or failure event
+		function createAccount(user, pass){
+			socket.emit('account create attempt', { username: user, password: pass });
+		}
+
+		//sends user login information to the server
+		//the server will send back either a success or failure event
+		function sendUser(user, pass){
 			socket.emit('login attempt', { username: user, password: pass });
 		}
 	}
+
 });
-
-
