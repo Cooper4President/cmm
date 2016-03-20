632786b5f432259eb4b5676902c31e5d976dfe7f
OliverEhrhardt <oliver.ehrhardt0@gmail.com>
2016-03-20 00:14:57 -0600

    added start panel and animations for login

--- client/js/home/home-main.js
+++ client/js/home/home-main.js
@@ -1,5 +1,10 @@
-define(['login'],function(login){
+define(['./login', './animations'],function(login, animation){
 	return function(){
+		animation.bottom('login-container').center('start');
 		login.init();
+
+		$(".login-op").on("click", function(event){
+			animation.top('start').center('login-container');
+		});
 	}
 });
