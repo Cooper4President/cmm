4ab0cd906a4796a20377c01442003f9c19550456
Cooper4President <Cooper4President@users.noreply.github.com>
2016-03-19 19:35:48 -0600

    fixed bug in index-data.js

--- client/js/home/index-data.js
+++ client/js/home/index-data.js
@@ -5,7 +5,7 @@ define(['jquery', './index-sockets'], function($, socket){
 			var username = $(".username").val();
 			var password = $(".password").val();
 			if(!/\s+/g.test(username)){ //test for valid user name
-				socket.sendUser(username, password);
+				sendUser(username, password);
 			}else{
 				alert("Username Invalid");
 			}
