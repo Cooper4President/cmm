1abca2904baf72fa80da1744471b1121032e67fb
Morgan Allen <moal8410@colorado.edu>
2016-04-27 12:52:09 -0600

    Adding JSDoc comments and files

--- client/js/app/misc/commandSockets.js
+++ client/js/app/misc/commandSockets.js
@@ -1,6 +1,30 @@
+/** @memberOf jQuery */
+/** @module commandSockets */
+/**
+ * @fileOverview Checks window to make sure elements are aligned properly
+ * @author Oliver Ehrhardt
+ * @author Morgan Allen
+*/
+
+
+/**
+ * Dependencies of the commandSockets module 
+ * @function
+ * @name Module Dependencies
+ * @param {Object} $ - Defines the jquery module dependency
+ * @param {Object} io - defines the socket.io module dependency
+ * @return {Functions} - Returns the inner return function
+ */
 define(['jquery', 'socket_io'], function($, io){
 	var socket = io();
-
+    /**
+     * Defines the query from the wolfram socket to the wolfram alpha servers
+     * @function
+     * @name ReturnFunction
+     * @param {String} inp - The raw user input string
+     * @param {AnonymousFunction} callback - callback function once the commands have been executed and finished
+     * @return {Object} returns the wolfram alpha query callback
+     */
 	return {
         wolframQuery: function(inp, callback) {
             socket.emit('wolfram', inp);
